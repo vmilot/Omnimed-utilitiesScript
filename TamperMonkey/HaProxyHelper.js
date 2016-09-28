c309f9f1523658394810e8e655dd79343f7719e3
Francis <fcorriveau@omnimed.com>
2016-09-28 11:30:53 -0400

    Create HaProxyHelper.js

---
+++ TamperMonkey/HaProxyHelper.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Proxy Shift Click
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        http://*/stats*
+// @grant        none
+// @require      https://code.jquery.com/jquery-2.1.4.min.js
+// @require      http://nylen.github.io/shiftcheckbox/jquery.shiftcheckbox.js
+// ==/UserScript==
+
+$ = jQuery;
+
+$(function() {
+    $(':checkbox').shiftcheckbox();
+});
+
+if (window.location.host == "addr.xx.xx.xx:yy") {
+  $('form').each(function(idx, e) {
+      if (idx < 10) {
+          $(e).hide();
+      }
+  });
+}
