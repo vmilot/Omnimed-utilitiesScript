b7e5719cda417eddc84150867ba84ca7d16df0f3
Gabriel Girard <ggirard@omnimed.com>
2016-11-24 13:28:42 -0500

    [#134929369] Correction if imbriquer

--- TamperMonkey/xenOrchestra.js
+++ TamperMonkey/xenOrchestra.js
@@ -35,17 +35,15 @@ document.onclick = function() {
                     blockSnapshot();
                 }
             }
-            if (document.getElementsByClassName("xo-icon-add fa-1x fa-fw")[0]) {
-                if (document.getElementsByClassName("xo-icon-add fa-1x fa-fw")[0].parentElement.childNodes[4]) {
-                    var text = document.getElementsByClassName("xo-icon-add fa-1x fa-fw")[0].parentElement.childNodes[4].firstChild.innerHTML;
-                    if (text.indexOf("snapshot") > 0) {
-                        if (document.getElementById("block2")) {
-                            document.getElementById("block2").remove();
-                        }
-                        document.getElementsByClassName("xo-icon-add fa-1x fa-fw")[0].parentElement.parentElement.insertAdjacentHTML('afterbegin', ("<div id='block2' onclick='document.showPopup()' style='height: 51px; width: 209px; position: absolute; left: 1480px;'></div>"));
-                        var dim = document.getElementsByClassName("xo-icon-add fa-1x fa-fw")[0].parentElement.getBoundingClientRect();
-                        document.getElementById("block2").style.left = dim.left - dim.right + dim.left;
+            if (document.getElementsByClassName("xo-icon-add fa-1x fa-fw")[0] && document.getElementsByClassName("xo-icon-add fa-1x fa-fw")[0].parentElement.childNodes[4]) {
+                var text = document.getElementsByClassName("xo-icon-add fa-1x fa-fw")[0].parentElement.childNodes[4].firstChild.innerHTML;
+                if (text.indexOf("snapshot") > 0) {
+                    if (document.getElementById("block2")) {
+                        document.getElementById("block2").remove();
                     }
+                    document.getElementsByClassName("xo-icon-add fa-1x fa-fw")[0].parentElement.parentElement.insertAdjacentHTML('afterbegin', ("<div id='block2' onclick='document.showPopup()' style='height: 51px; width: 209px; position: absolute; left: 1480px;'></div>"));
+                    var dim = document.getElementsByClassName("xo-icon-add fa-1x fa-fw")[0].parentElement.getBoundingClientRect();
+                    document.getElementById("block2").style.left = dim.left - dim.right + dim.left;
                 }
             }
         }
@@ -71,14 +69,12 @@ document.showPopup = function() {
     if (document.getElementById("popup")) {
         document.getElementById("popup").remove();
     }
-    if (document.getElementsByClassName('header-title')[0]){
-        if (document.getElementsByClassName('header-title')[0].childNodes[1]) {
-            if (document.getElementsByClassName('header-title')[0].childNodes[1].childNodes[1]) {
-                if (document.getElementsByClassName('header-title')[0].childNodes[1].childNodes[1].childNodes[4]) {
-                    var url = document.getElementsByClassName('header-title')[0].childNodes[1].childNodes[1].childNodes[4].href;
-                    document.getElementById("xo-app").insertAdjacentHTML('afterbegin', ("<div id='popup' style='height: 800px; width: 1840px; position: absolute; background-color: dimgrey; z-index: 3000; top: 70px; left: 50px;'><div style='font-size: -webkit-xxx-large;font-weight: bolder;float: right;margin-right: 10px;' onclick='document.closePopup()'>X</div><div style=' text-align: center; font-size: -webkit-xxx-large; margin-top: 40px; '>LE HOST A ASSEZ D'ESPACE???? <br><button onclick='document.snapshot()' style='margin-right: 10px;'>OUI</button><button onclick='document.closePopup()'>NON</button></div><iframe src='" + url + "/storage' style=' width: 98%; margin-left: 1%; height: 74%; margin-top: 10px;'></iframe></div>"));
-                }
-            }
-        }
+    if (document.getElementsByClassName('header-title')[0] && 
+        document.getElementsByClassName('header-title')[0].childNodes[1] && 
+        document.getElementsByClassName('header-title')[0].childNodes[1].childNodes[1]&& 
+        document.getElementsByClassName('header-title')[0].childNodes[1].childNodes[1].childNodes[4]) {
+        var url = document.getElementsByClassName('header-title')[0].childNodes[1].childNodes[1].childNodes[4].href;
+        document.getElementById("xo-app").insertAdjacentHTML('afterbegin', ("<div id='popup' style='height: 800px; width: 1840px; position: absolute; background-color: dimgrey; z-index: 3000; top: 70px; left: 50px;'><div style='font-size: -webkit-xxx-large;font-weight: bolder;float: right;margin-right: 10px;' onclick='document.closePopup()'>X</div><div style=' text-align: center; font-size: -webkit-xxx-large; margin-top: 40px; '>LE HOST A ASSEZ D'ESPACE???? <br><button onclick='document.snapshot()' style='margin-right: 10px;'>OUI</button><button onclick='document.closePopup()'>NON</button></div><iframe src='" + url + "/storage' style=' width: 98%; margin-left: 1%; height: 74%; margin-top: 10px;'></iframe></div>"));
     }
+
 };
