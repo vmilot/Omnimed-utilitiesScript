ddbd0c753b6f7fb6d882111ecc67326c91403596
Gabriel Girard <ggirard@omnimed.com>
2016-12-01 16:43:04 -0500

    Update kgToLbs.js

--- TamperMonkey/kgToLbs.js
+++ TamperMonkey/kgToLbs.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         kg to lbs
 // @namespace    https://www.omnimed.com/
-// @version      0.1
+// @version      0.2
 // @description  Display lbs data
 // @author       Gabriel Girard
 // @match        https://www.omnimed.com/omnimed/do/dashboard/patientDashboard*
@@ -14,11 +14,14 @@
 
     function getWeightInImperial() {
         setTimeout(function() {
+            var height;
             var weight;
             var vitalSign = $('div[class="vitalSignUnit vitalSignUnitSumaryBox ng-binding"]');
             $.each(vitalSign,function(e) {
                 if (vitalSign[e].innerHTML === "kg") {
                     weight = vitalSign[e];
+                } else if (vitalSign[e].innerHTML === "m") {
+                    height = vitalSign[e];
                 }
             });
 
@@ -26,6 +29,14 @@
                 var imperial = kToLbs($(weight).prev()[0].innerHTML);
 
                 $(weight).parent().attr('title', 'Pounds+Ounces : ' + imperial.pounds + ' lb  ' + imperial.ounces + ' oz \nPounds: ' + imperial.combine + ' lb' );
+            }
+
+            if (height) {
+                var value = $(height).prev()[0].innerHTML;
+                var inches = (value*39.3700787).toFixed(0);
+                var feet = Math.floor(inches / 12);
+                inches %= 12;
+                $(height).parent().attr('title', feet + ' ft  ' + inches + 'in' );
             }
 
             getWeightInImperial();
