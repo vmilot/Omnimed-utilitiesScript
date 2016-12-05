bbb2be6c0a7782d10594f996d253b52a6def8c14
Samuel Henault <shenault@omnimed.com>
2016-12-05 08:00:20 -0500

    Gestion du pourcentage dans tous les rapports

--- TamperMonkey/CucumberReports.js
+++ TamperMonkey/CucumberReports.js
@@ -4,11 +4,11 @@
 // @version      0.1
 // @description  Display cucumber report total percent
 // @author       You
-// @match        https://jenkins.omnimed.com/job/cucumber-java-2/*/cucumber-html-reports/feature-overview.html
+// @match        https://jenkins.omnimed.com/job/cucumber*/*/cucumber-html-reports/feature-overview.html
 // @grant        none
 // ==/UserScript==
 (function() {
     'use strict';
     jQuery('table.table-bordered thead tr').append('<th>Percent</th>');
-    jQuery('table.table-bordered tbody tr.info').append('<td>'+ parseFloat(parseFloat(jQuery('td[data-column=6]').text() / jQuery('td[data-column=5]').text())*100).toFixed(2) +'%</td>');
+    jQuery('table.table-bordered tbody tr.info').append('<td>'+ parseFloat(parseFloat(jQuery('td[data-column=2]').text() / jQuery('td[data-column=1]').text())*100).toFixed(2) +'%</td>');
 })();
