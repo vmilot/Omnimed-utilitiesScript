85c41756227809d1f9f0da4b0dc314014b2b58a1
Gabriel Girard <ggirard@omnimed.com>
2017-02-27 15:07:34 -0500

    Update PivotalTracker.js

--- TamperMonkey/PivotalTracker.js
+++ TamperMonkey/PivotalTracker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pivotal Tracker Enhanced
 // @namespace    https://www.pivotaltracker.com/
-// @version      0.19
+// @version      0.20
 // @description  Pivotal Tracker enhanced for Omnimed
 // @author       Gabriel Girard
 // @match        https://www.pivotaltracker.com/*
@@ -31,12 +31,14 @@ $( document ).keypress(function( event ) {
 
 $( document ).ready(function() {
     $("<style type='text/css'> .analyseIcon:before{ background-image:url(https://cdn2.iconfinder.com/data/icons/pixel-perfect-at-24px-volume-7/24/2190-128.png) !important;} </style>").appendTo("head");
+    $("<style type='text/css'> .heartIcon:before{ background-image:url(http://downloadicons.net/sites/default/files/heart-icon-62329.png) !important;} </style>").appendTo("head");
     updateAnalyse();
 });
 
 function updateAnalyse() {
     setTimeout(function() {
         $('.bug,.chore,.feature').find('.labels.post').find("a:contains('analyse')").parent().parent().parent().children('.meta').addClass('analyseIcon');
+        $('.bug,.chore,.feature').find('.labels.post').find("a:contains('coeur')").parent().parent().parent().children('.meta').addClass('analyseIcon');
         updateAnalyse();
     }, 5000);
 }
