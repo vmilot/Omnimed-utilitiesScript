4eb49fcb18a747572fc3ad3ad4475d3baa6b5851
Nicolas Guillette <nguillette@omnimed.com>
2017-05-24 17:39:35 -0400

    Update CucumberReports.js

--- TamperMonkey/CucumberReports.js
+++ TamperMonkey/CucumberReports.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cucumber pimper
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Pimp cucumber reports
 // @author       mquiron, mcormier, nguillet 
 // @match        https://jenkins.omnimed.com/job/*/cucumber-html-reports/*overview-tags.html
