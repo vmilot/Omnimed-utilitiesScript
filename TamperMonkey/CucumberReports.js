c5038879020dcb574c2edc34d0ce4f87aaff7562
Valérie Milot <vmilot@omnimed.com>
2018-07-17 11:06:09 -0400

    Update CucumberReports.js

--- TamperMonkey/CucumberReports.js
+++ TamperMonkey/CucumberReports.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cucumber pimper
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.2
 // @description  Pimp cucumber reports
 // @author       mquiron, mcormier, nguillet, shenault
 // @match        https://jenkins.omnimed.com/job/*/cucumber-html-reports/*overview-tags.html
